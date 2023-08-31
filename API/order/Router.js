215930d6c4c31c01d4ac506ce9c21a968340c96e
Mahnoor-Yameen <mahnooryameen12@gmail.com>
2023-08-31 01:37:19 -0700

    FEAT: complete app done

---
+++ API/order/Router.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const router=express.Router()
+const { placeorder, allOrders,trackOrder } = require('./Controller')
+
+
+router.post('/place-order', placeorder)  //
+router.get('/get-all-orders', allOrders)   //all orders
+router.get('/track-order/:_id', trackOrder)   //order track by id
+
+module.exports=router
